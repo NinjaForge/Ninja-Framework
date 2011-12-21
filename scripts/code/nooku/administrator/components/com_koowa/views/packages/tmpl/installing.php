9d07aa5bc721dd60c33180e535be63dd634a987a
Stian Didriksen <stipsan@me.com>
2011-12-21 02:52:24 +0100

    Revert "GitHub for Mac: Throw-away commit."
    This reverts commit 37302de16c91f87978707979996edbf63817a9a5.

--- scripts/code/nooku/administrator/components/com_koowa/views/packages/tmpl/installing.php
+++
@@ -1,6 +0,0 @@
-<? /* $Id: installing.php 917 2011-09-19 12:15:44Z stiandidriksen $ */ ?>
-<? defined( 'KOOWA' ) or die( 'Restricted access' ) ?>
-<h2 class="working">
-    <?= sprintf(@text('Installing %s'), $package) ?>
-    <? $b = $total + $i - 1; ?><span><?= sprintf(@text('Package %d of %d'), $i, $b) ?></span>
-</h2>
--- scripts/code/nooku/administrator/components/com_koowa/views/packages/tmpl/unpacking.php
+++
@@ -1,6 +0,0 @@
-<? /* $Id: unpacking.php 917 2011-09-19 12:15:44Z stiandidriksen $ */ ?>
-<? defined( 'KOOWA' ) or die( 'Restricted access' ) ?>
-<h2 class="working">
-    <?= sprintf(@text('Unpacking %s'), $package) ?>
-    <? $b = $total; ?><span><?= sprintf(@text('Package %d of %d'), $i, $b) ?></span>
-</h2>
