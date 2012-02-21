88fb2b1ff46a13952698fdbac9e7d478e260c8a2
Stian Didriksen <stipsan@me.com>
2012-02-21 18:18:50 +0100

    updating koowa snapshot

--- code/nooku/administrator/components/com_koowa/models/dashboards.php
+++ code/nooku/administrator/components/com_koowa/models/dashboards.php
@@ -1,10 +1,10 @@
 <?php
 /**
- * @version     $Id: dashboards.php 917 2011-09-19 12:15:44Z stiandidriksen $
+ * @version     $Id: dashboards.php 1481 2012-02-10 01:46:24Z johanjanssens $
  * @category    Nooku
  * @package     Nooku_Components
  * @subpackage  Koowa
- * @copyright   Copyright (C) 2010 Timble CVBA and Contributors. (http://www.timble.net)
+ * @copyright   Copyright (C) 2010 - 2012 Timble CVBA and Contributors. (http://www.timble.net)
  * @license     GNU GPLv3 <http://www.gnu.org/licenses/gpl.html>
  * @link        http://www.nooku.org
  */
--- code/nooku/administrator/components/com_koowa/views/packages/tmpl/default.php
+++ code/nooku/administrator/components/com_koowa/views/packages/tmpl/default.php
@@ -1,5 +1,15 @@
-<? /* $Id: default.php 917 2011-09-19 12:15:44Z stiandidriksen $ */ ?>
-<? defined( 'KOOWA' ) or die( 'Restricted access' ) ?>
+<?php
+/**
+ * @version     $Id: default.php 1481 2012-02-10 01:46:24Z johanjanssens $
+ * @category	Nooku
+ * @package     Nooku_Components
+ * @subpackage  Koowa
+ * @copyright   Copyright (C) 2011 - 2012 Timble CVBA and Contributors. (http://www.timble.net).
+ * @license     GNU GPLv3 <http://www.gnu.org/licenses/gpl.html>
+ * @link        http://www.nooku.org
+ */
+defined('KOOWA') or die( 'Restricted access' ); ?>
+
 <h2 class="working">
     <?= $total === 1 ? @text('Found 1 install package') : sprintf(@text('Found %d packages'), $total) ?>
 </h2>
