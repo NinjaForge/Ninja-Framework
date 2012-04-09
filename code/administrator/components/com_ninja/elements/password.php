a67082cd803c46e0ec672ca087a4cee8dd702fe3
Richie Mortimer <richie@ninjaforge.com>
2012-04-09 12:57:08 +0100

    lets have a password element

---
+++ code/administrator/components/com_ninja/elements/password.php
@@ -0,0 +1,19 @@
+<?php defined( 'KOOWA' ) or die( 'Restricted access' );
+/**
+ * @category	Napi
+ * @package		Napi_Parameter
+ * @copyright	Copyright (C) 2007 - 2011 NinjaForge. All rights reserved.
+ * @license		GNU GPLv3 <http://www.gnu.org/licenses/gpl.html>
+ * @link     	http://ninjaforge.com
+ */
+
+class NinjaElementPassword extends NinjaElementAbstract
+{
+	public function fetchElement($name, $value, &$node, $control_name)
+	{
+		$size = ( $node->attributes('size') ? 'size="'.$node->attributes('size').'"' : '' );
+		$class = ( $node->attributes('class') ? 'class="'.$node->attributes('class').'"' : 'class="value"' );
+
+		return '<input type="password" name="'.$control_name.'['.$name.']" id="'.$control_name.$name.'" value="'.$value.'" '.$class.' '.$size.' />';
+	}
+}
